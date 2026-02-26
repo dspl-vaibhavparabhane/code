e7ca5192eaec3746ec8fd320397a55b1f63f53e4
dspl-vaibhavparabhane <vaibhav.parabhane@devangles.com>
2026-02-26 09:31:39 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,1 @@
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from app import create_app
-
-env = os.getenv("FLASK_ENV", "development")
-
-app = create_app(env)
-
-if __name__ == "__main__":
-    # Use gunicorn in production: gunicorn -w 4 "app:create_app()"
-    app.run(
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 5000)),
-        debug=os.getenv("FLASK_ENV") == "development"
-    )
