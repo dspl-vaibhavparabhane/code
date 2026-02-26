851c79af5eb68de21438c2ad959e77d7857a2b84
dspl-vaibhavparabhane <vaibhav.parabhane@devangles.com>
2026-02-26 09:25:17 +0530

    Delete app directory

--- app/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-
-
-from .auth_controller import AuthController
-from .asset_controller import AssetController
-from .conference_room_controller import ConferenceRoomController
-from .booking_controller import BookingController
-
-__all__ = ["AuthController", "AssetController", "UserController", "ConferenceRoomController", "BookingController"]
